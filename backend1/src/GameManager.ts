ee6ba0a08edf7c0691c26abf66e90ba644875173
Riteek Rakesh <riteek7.12@gmail.com>
2024-05-16 22:55:55 +0530

    game manager setup begin

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,18 +1,22 @@
+import WebSocket from "ws";
+// User, Game
 export class GameManager {
     private games: Game();
+    private pendingUser: WebSocket;
+    private users: WebSocket[];
     constructor() {
         this.games = [];
     }
 
     addUser(socket: WebSocket) {
-        
+        this.users.push(socket)
     }
     removeUser(socket: WebSocket) {
 
     }
 
     private handleMessage () {
-        
+
     }
 
 }
